d0e2e23a188c0e0642bc0f27ee7d3b5cbf7b4184
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2018-08-20 23:51:02 -0300

    Site FAQ

--- routes/web.php
+++ routes/web.php
@@ -76,6 +76,7 @@ Route::group([
 {
     Route::get('/', 'HomeController@index')->name('site.index');
     Route::get('/home', 'HomeController@index')->name('site.home');
+    Route::get('/faq', 'FaqController@index')->name('site.faq');
 });
 
 
